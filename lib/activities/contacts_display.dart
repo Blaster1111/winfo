4777dc9d9aeb4fa4ed9903098655b62b7e73b869
unknown <dhairudu0711@gmail.com>
2023-08-15 15:48:05 +0530

    ContactCommit

--- lib/activities/contacts_display.dart
+++ lib/activities/contacts_display.dart
@@ -1,6 +1,7 @@
+import 'dart:typed_data';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:contacts_service/contacts_service.dart';
-import 'package:permission_handler/permission_handler.dart';
+import 'package:flutter_contacts/flutter_contacts.dart';
 
 class ContactDisplay extends StatefulWidget {
   const ContactDisplay({super.key});
@@ -10,103 +11,53 @@ class ContactDisplay extends StatefulWidget {
 }
 
 class _ContactDisplayState extends State<ContactDisplay> {
-  List<Contact> contacts = [];
-
-  bool isLoading = true;
-
+  List<Contact>? contacts;
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
-    getContactPermission();
+    getContact();
   }
 
-  Future<void> getContactPermission() async {
-    var status = Permission.contacts.request();
-
-    if (status == true) {
-      fetchContacts();
-    } else {
-      await Permission.contacts.request();
+  void getContact() async {
+    if (await FlutterContacts.requestPermission()) {
+      contacts = await FlutterContacts.getContacts(
+          withProperties: true, withPhoto: true);
+      print(contacts);
+      setState(() {});
     }
-  }
-
-  void fetchContacts() async {
-    contacts = await ContactsService.getContacts();
-
-    setState(() {
-      isLoading = false;
-    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Contacts"),
-      ),
-      body: isLoading
-          ? Center(
-              child: CircularProgressIndicator(),
-            )
-          : ListView.builder(
-              itemCount: contacts.length,
-              itemBuilder: (context, index) {
-                return ListTile(
-                  leading: Container(
-                    height: 30,
-                    width: 30,
-                    alignment: Alignment.center,
-                    decoration: BoxDecoration(
-                      boxShadow: [
-                        BoxShadow(
-                          blurRadius: 7,
-                          color: Colors.white.withOpacity(0.1),
-                          offset: const Offset(-3, -3),
-                        ),
-                        BoxShadow(
-                          blurRadius: 7,
-                          color: Colors.black.withOpacity(0.7),
-                          offset: const Offset(3, 3),
-                        ),
-                      ],
-                      borderRadius: BorderRadius.circular(6),
-                      color: Color(0xff262626),
-                    ),
-                    child: Text(
-                      contacts[index].givenName![0],
-                      style: TextStyle(
-                        fontSize: 23,
-                        color: Colors.black,
-                        fontFamily: "Poppins",
-                        fontWeight: FontWeight.w500,
-                      ),
-                    ),
-                  ),
-                  title: Text(
-                    contacts[index].givenName!,
-                    maxLines: 1,
-                    overflow: TextOverflow.ellipsis,
-                    style: TextStyle(
-                      fontSize: 16,
-                      color: Colors.cyanAccent,
-                      fontFamily: "Poppins",
-                      fontWeight: FontWeight.w500,
-                    ),
-                  ),
-                  subtitle: Text(
-                    contacts[index].phones![0].value!,
-                    style: TextStyle(
-                      fontSize: 11,
-                      color: const Color(0xffC4c4c4),
-                      fontFamily: "Poppins",
-                      fontWeight: FontWeight.w400,
-                    ),
-                  ),
-                  horizontalTitleGap: 12,
-                );
-              },
-            ),
-    );
+        appBar: AppBar(
+          title: const Text(
+            "Your Contacts",
+            style: TextStyle(color: Colors.blue),
+          ),
+          centerTitle: true,
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+        ),
+        body: (contacts) == null
+            ? Center(child: CircularProgressIndicator())
+            : ListView.builder(
+                itemCount: contacts!.length,
+                itemBuilder: (BuildContext context, int index) {
+                  Uint8List? image = contacts![index].photo;
+                  String num = (contacts![index].phones.isNotEmpty)
+                      ? (contacts![index].phones.first.number)
+                      : "--";
+                  return ListTile(
+                      leading: (contacts![index].photo == null)
+                          ? const CircleAvatar(child: Icon(Icons.person))
+                          : CircleAvatar(backgroundImage: MemoryImage(image!)),
+                      title: Text(
+                          "${contacts![index].name.first} ${contacts![index].name.last}"),
+                      subtitle: Text(num),
+                      onTap: () {});
+                },
+              ));
   }
 }
